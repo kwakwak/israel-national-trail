5030475bd285bc229bb083557433c3274ad4e591
kwakwak <ronihcohen@gmail.com>
2014-03-10 17:23:35 +0200

    many

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,64 +5,9 @@
 angular.module('myApp.controllers', [])
    .controller('HomeCtrl', ['$scope', 'syncData','$routeParams','$log','$location', function($scope, syncData,$routeParams,$log,$location) {
 
+        $scope.sections = syncData('israel-national-trail');
 
-        $scope.allEvents = syncData('events');
-        $scope.allEvents.$on('loaded', function()
-        {
-            $scope.keys = $scope.allEvents.$getIndex();
-            $scope.firstDate = $scope.keys[0]; // first date in server;
-
-            $scope.today = function(){
-                var today = new Date();
-                return today.getFullYear() +
-                "-" +
-                ('0' + (today.getMonth()+1)).slice(-2) +
-                "-" +
-                ('0' + (today.getDate())).slice(-2);
-            };
-
-
-            $scope.lastKey = $scope.keys.length-1;
-            $scope.lastDate =  $scope.keys[$scope.lastKey]; // last date in server
-
-            $scope.urlDate = $location.search().date;
-            if (typeof($scope.urlDate) == 'undefined'){
-                $scope.date =  $scope.today();
-                $location.search('date', $scope.date);
-            } else {
-                $scope.date = $scope.urlDate;
-            }
-
-            $scope.reloadDate =function(date){
-                $scope.lastDate==date?$scope.hideNext=true:$scope.hideNext=false;
-                $scope.firstDate==date?$scope.hidePrev=true:$scope.hidePrev=false;
-                $scope.eventsInDate = syncData('events').$child(date);
-                $scope.currentKey = $scope.keys.indexOf(date);
-                $scope.date =  date;
-            };
-
-            $scope.reloadDate($scope.date);
-
-            // if cant find a date in array reload to last
-            if ($scope.currentKey=='-1') {
-                $scope.date =  $scope.lastDate;
-                $location.search('date', $scope.date);
-                $scope.reloadDate($scope.date);
-            }
-
-            $scope.goTo = function(dir){
-                if (dir=="prev") $scope.currentKey--;
-                else if (dir=="next") $scope.currentKey++;
-                var newDate =  $scope.keys[$scope.currentKey];
-                $scope.reloadDate(newDate);
-                $location.search('date', newDate);
-            };
-
-            $scope.red = function(element){
-                if (element==$scope.date) return true;
-                else  return false;
-            };
-        });
+ 
    }])
   .controller('ChatCtrl', ['$scope', 'syncData', function($scope, syncData) {
       $scope.newMessage = null;
--- app/js/filters.js
+++ app/js/filters.js
@@ -3,34 +3,16 @@
 /* Filters */
 
 angular.module('myApp.filters', [])
-   .filter('interpolate', ['version', function(version) {
-      return function(text) {
-         return String(text).replace(/\%VERSION\%/mg, version);
-      }
-   }]).filter('startFrom', function() {
-    return function(input, start) {
-        if (typeof (input) != "undefined") {
-            start = +start; //parse to int
-            return input.slice(start);
-        }
-    }
+   .filter('clean', function() {  // clean items without paticipants.
+      return function(list) {
+        var k, out = [];
+        for (k in list) if (list[k].participants) out.push(list[k]);
+        return out;
+      };
     })
-   .filter('reverse', function() {
-      function toArray(list) {
-         var k, out = [];
-         if( list ) {
-            if( angular.isArray(list) ) {
-               out = list;
-            }
-            else if( typeof(list) === 'object' ) {
-               for (k in list) {
-                  if (list.hasOwnProperty(k)) { out.push(list[k]); }
-               }
-            }
-         }
-         return out;
-      }
-      return function(items) {
-         return toArray(items).slice().reverse();
+   .filter('part', function() {  // clean items without paticipants.
+      return function(list) {
+        console.dir (list);
+        return list;
       };
    });
