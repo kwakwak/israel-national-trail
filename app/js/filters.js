c02964c58c720e523c14a9149555d5017120284a
kwakwak <ronihcohen@gmail.com>
2014-03-12 15:20:57 +0200

    not completed sections

--- app/js/filters.js
+++ app/js/filters.js
@@ -13,7 +13,7 @@ angular.module('myApp.filters', [])
    .filter('clean', function() {  // clean items without paticipants.
       return function(list) {
         var k, out = [];
-        for (k in list) if (list[k].participants) out.push(list[k]);
+        for (k in list) if (typeof(list[k]) === 'object') out.push(list[k]);
         return out;
       };
    });
